db3aa4efe3ecbab12ff6a21e2ab257a55c9c0829
nikhil-223 <113900755+nikhil-223@users.noreply.github.com>
2023-03-10 20:16:29 +0530

    currency drop updated

--- src/components/chart/CurrencyDrop.jsx
+++ src/components/chart/CurrencyDrop.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import CurrencyItem from './CurrencyItem'
 import {IoMdArrowDropdown} from 'react-icons/io'
 
 const CurrencyDrop = () => {
+	const [dropdownName, setDropdownName] = useState('USD $')
     const arr = [{ itemname: "rahul" }, { itemname: "nikhil" }];
     const showList=()=>{
-        document.getElementById("currencyDroplist").style.display='flex';
+		const dropListState =
+			document.getElementById("currencyDroplist").style.display;
+			dropListState === "none"
+				? (document.getElementById("currencyDroplist").style.display = "flex")
+				: (document.getElementById("currencyDroplist").style.display = "none");
     }
+	const handleChange=(e)=>{
+		setDropdownName(e.target.value)
+	}
   return (
-		<div className="flex justify-between gap-2 px-2 py-2 m-1 rounded-lg bg-dropdownBoxDark text-white">
-			<input type="text" className="drop-input w-4/5 bg-transparent " value='gg'/>
+		<div className="flex justify-between gap-2 px-2 py-2 m-1 my-2 rounded-lg bg-dropdownBoxDark text-white">
+			<input type="text" className="drop-input w-4/5 p-2 bg-transparent focus:outline-none " value={dropdownName} onChange={handleChange}/>
 			<span className="w-1/5 flex justify-center items-center text-5xl" onClick={showList}>
 				<IoMdArrowDropdown/>
 			</span>
-			<div id='currencyDroplist' className="droplist absolute translate-y-12 bg-slate-400 w-28 h-48 flex-col rounded overflow-scroll hidden">
+			<div id='currencyDroplist' className="droplist absolute translate-y-12 bg-dropdownListDark w-28 h-48 flex-col rounded overflow-scroll" style={{display:'none'}}>
 				{arr.map((item) => {
 					return <CurrencyItem key={item.itemname} itemname={item.itemname} />;
 				})}
