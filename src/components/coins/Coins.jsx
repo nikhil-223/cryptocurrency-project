26e1b9c5ffccc6ed741a05b3e0b0071fd48de93f
himanshu308 <120033070+himanshu308@users.noreply.github.com>
2023-03-11 14:15:23 +0530

    Update Coins.jsx

--- src/components/coins/Coins.jsx
+++ src/components/coins/Coins.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CoinItem from "./CoinItem";
+import {AiOutlineSearch} from "react-icons/ai"
 
 const Coins = () => {
 	const arr = [
@@ -20,7 +21,10 @@ const Coins = () => {
 		<div
 			id="coins"
 			className={` col-span-2 row-span-5 rounded-xl bg-boxDark text-white`}>
-			<div className="h-12">title</div>
+			<div className="h-12">
+				<div className="flex item-center font-medium justify-start h-10 " ><AiOutlineSearch/><br /></div><div className="flex items-center font-medium justify-center">Search</div>
+	             <div></div>
+			</div>
 			<div className=" bg-slate-400 h-4/5 overflow-scroll">
 				{arr.map((item) => {
 					return <CoinItem itemname={item.itemname} title={item.title} />;
