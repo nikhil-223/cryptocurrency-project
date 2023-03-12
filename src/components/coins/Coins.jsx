38fe2b6782cafb2e46657c3b43916e1a0eb11fc1
himanshu308 <120033070+himanshu308@users.noreply.github.com>
2023-03-12 22:41:10 +0530

    Update Coins.jsx

--- src/components/coins/Coins.jsx
+++ src/components/coins/Coins.jsx
@@ -22,7 +22,7 @@ const Coins = () => {
 			id="coins"
 			className={` col-span-2 row-span-5 rounded-xl bg-boxDark text-white`}>
 			<div className="h-12">
-				<div className="flex item-start grid font-medium justify-start h-90 w-44 " ><AiOutlineSearch/></div><div className="flex items-center font-medium justify-center h-11 drop-shadow-sm">Search</div>
+				<div className=" m-3 p-3"><AiOutlineSearch/></div><div className="p-8 m-1 h-50 w-50 justify-start ">Search</div>
 	             <div></div>
 			</div>
 			<div className=" bg-slate-400 h-4/5 overflow-scroll">
