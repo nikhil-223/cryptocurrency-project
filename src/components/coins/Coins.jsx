d15aab6771d632d6e1300808ce480ab075299aaf
himanshu308 <120033070+himanshu308@users.noreply.github.com>
2023-03-13 12:50:15 +0530

    Update Coins.jsx

--- src/components/coins/Coins.jsx
+++ src/components/coins/Coins.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CoinItem from "./CoinItem";
 import {AiOutlineSearch} from "react-icons/ai"
+import { MdLightMode } from "react-icons/md";
 
 const Coins = () => {
 	const arr = [
@@ -21,11 +22,14 @@ const Coins = () => {
 		<div
 			id="coins"
 			className={` col-span-2 row-span-5 rounded-xl bg-boxDark text-white`}>
-			<div className="h-12">
-				<div className=" m-3 p-3"><AiOutlineSearch/></div><div className="p-8 m-1 h-50 w-50 justify-start ">Search</div>
-	             <div></div>
-			</div>
-			<div className=" bg-slate-400 h-4/5 overflow-scroll">
+			<div className=" h-12 w-full flex items-center justify-start ">
+				<div className=" m-3 text-xl"><AiOutlineSearch size={25}/></div>
+				
+				<input className="h-full w-3/5 bg-boxDark outline-none " type="text" placeholder="Search by Coin"/>
+				
+	             <div className=" ml-auto w-12 text-xl flex item-end bg-transparent text-fuchsia-600" >< MdLightMode size={30}/></div>
+			</div> 
+			<div className=" bg-slate-400 h-4/5 overflow-scroll ">
 				{arr.map((item) => {
 					return <CoinItem itemname={item.itemname} title={item.title} />;
 				})}
