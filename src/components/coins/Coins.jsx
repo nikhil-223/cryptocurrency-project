83cabac94bf9effde1b3b1edf3df900d218a0a6d
himanshu308 <120033070+himanshu308@users.noreply.github.com>
2023-03-13 12:57:30 +0530

    Revert "Himanshucontri"

--- src/components/coins/Coins.jsx
+++ src/components/coins/Coins.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCoins } from "../../api";
 import CoinItem from "./CoinItem";
-import {AiOutlineSearch} from "react-icons/ai"
-import { MdLightMode } from "react-icons/md";
 
 const Coins = () => {
 
@@ -20,20 +18,17 @@ const Coins = () => {
 	}, [first]);
 
 	return (
-		<div
-			id="coins"
-			className={` col-span-2 row-span-5 rounded-xl bg-boxDark text-white`}>
-			<div className=" h-12 w-full flex items-center justify-start ">
-				<div className=" m-3 text-xl"><AiOutlineSearch size={25}/></div>
-				
-				<input className="h-full w-3/5 bg-boxDark outline-none " type="text" placeholder="Search by Coin"/>
-				
-	             <div className=" ml-auto w-12 text-xl flex item-end bg-transparent text-fuchsia-600" >< MdLightMode size={30}/></div>
-			</div> 
-			<div className=" bg-slate-400 h-4/5 overflow-scroll ">
-				{arr.map((item) => {
-					return <CoinItem itemname={item.itemname} title={item.title} />;
-				})}
+		<>
+			<div className="h-12">title</div>
+			<div className="coinlist py-4 h-5/6 overflow-scroll">
+				{coins? coins.map((item,index) => {
+					return <CoinItem key={index} name={item.name} 
+			current_price={item.current_price}
+			market_cap_change_percentage_24h={item.market_cap_change_percentage_24h}
+			image={item.image}
+			symbol={item.symbol} />;
+				}):"loading..." 
+			}
 			</div>
 		</>
 	);
