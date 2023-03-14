dd3a69a7208b0f5a9b6c25326a2b5145f0a09a67
Vijaypal2000 <118725551+Vijaypal2000@users.noreply.github.com>
2023-03-14 11:06:10 +0530

    added portfolio chart

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import PieChart from './PieChart';
 
+import {useSelector} from 'react-redux'
+
 const Portfolio = () => {
+	const coins=useSelector((state)=>{
+		return state.coins.data;
+	  })
   return (
 	<>
 		<div className='flex justify-between items-center row-span-1 '>
 				<div className=' text-lg font-bold'>Portfolio</div>
 				<div>price:500$</div>
 			</div>
-			<div className=' row-span-5 '>
-				<PieChart/>
+			<div className=' row-span-5 mr-12'>
+				{coins[0] && <PieChart/>}
 			</div>
 	</>
 	);
