c84fcc3e0c331ce447c54a8213c87bb6614d5dfe
nikhil-223 <113900755+nikhil-223@users.noreply.github.com>
2023-03-24 15:43:17 +0530

    exchange components  comments

--- src/components/exchange/BuyListItem.jsx
+++ src/components/exchange/BuyListItem.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+
 import { setBuyCoin, setBuyDropName } from "../../store/slices/DropSlice";
 import { useAppSelector } from "../../store/storeAccess";
 
 const BuyListItem = (props) => {
-	const dispatch = useDispatch(); // create a dispatch function to update the store
+	const dispatch = useDispatch(); 
 	const { name } = props; // extract the name from props
 	const {theme} =useAppSelector() // select the theme from the store
+
 	const handleClick = () => {
 		dispatch(setBuyCoin(name)); // set the selected coin to buy in the store
 		dispatch(setBuyDropName(name)); // set the name of the selected coin in the buy dropdown
 	};
+	
 	return (
 		// display the name of the coin in a div with a hover effect
 		<div
--- src/components/exchange/SellListItem.jsx
+++ src/components/exchange/SellListItem.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { setSellCoin, setSellDropName } from "../../store/slices/DropSlice";  // Importing Redux actions from DropSlice.js file
-import { useAppSelector } from "../../store/storeAccess"; // Importing a custom Redux hook from storeAccess.js file
+import { setSellCoin, setSellDropName } from "../../store/slices/DropSlice"; 
+import { useAppSelector } from "../../store/storeAccess"; 
 
 
 const SellListItem = (props) => {
-	const dispatch = useDispatch(); // Initializing the Redux dispatch function
+	const dispatch = useDispatch(); 
 	const { name } = props; // Destructuring the 'name' prop passed from parent component
 	const {theme} = useAppSelector() // Accessing the 'theme' state value from Redux store using custom hook 'useAppSelector'
+	
 	const handleClick = () => {
 		dispatch(setSellCoin(name)); // Dispatching the 'setSellCoin' action with the 'name' value passed as argument
 		dispatch(setSellDropName(name)); // Dispatching the 'setSellDropName' action with the 'name' value passed as argument
@@ -25,4 +26,4 @@ const SellListItem = (props) => {
 	);
 };
 
-export default SellListItem;  // Exporting the SellListItem component so that it can be used in other parts of the application
+export default SellListItem;  
